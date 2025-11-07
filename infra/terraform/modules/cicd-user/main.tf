19148742fa9e2e609effbf62cd86fbc78e494223
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-11-07 13:14:53 -0700

    fix: update cicd user s3 permissmions

--- infra/terraform/modules/cicd-user/main.tf
+++ infra/terraform/modules/cicd-user/main.tf
@@ -204,7 +204,8 @@ data "aws_iam_policy_document" "cicd_user_policy_document" {
       "s3:GetBucketRequestPayment",
       "s3:GetBucketLogging",
       "s3:GetLifecycleConfiguration",
-      "s3:GetReplicationConfiguration"
+      "s3:GetReplicationConfiguration",
+      "s3:GetEncryptionConfiguration"
     ]
     resources = ["*"]
   }
